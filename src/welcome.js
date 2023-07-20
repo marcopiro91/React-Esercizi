80686a11855465611119ff1adf1477b5e85a6544
Amrock <marcopiro91@gmail.com>
2023-07-20 16:56:41 +0200

    added exercise7

--- src/welcome.js
+++ src/welcome.js
@@ -5,8 +5,9 @@ export class Welcome extends React.Component {
     render() {
         return  <div>
                     <p>Welcome, {this.props.name}</p>    
-                    <Age age={this.props.age} />
+                    { this.props.age > 18 && <Age age={this.props.age} />}
                 </div>
     }
 }
 
+// Modify the Welcome component so that the Age component is rendered only if the age prop is greater than 18.
