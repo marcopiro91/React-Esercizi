49354339e6699f81857f0207ac0df83f9e561df0
Amrock <marcopiro91@gmail.com>
2023-07-20 14:52:25 +0200

    added exercise5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Welcome } from "./Welcome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+    render() {
+        return <Welcome name="marco" age="31" />
+    }
 }
 
-export default App;
