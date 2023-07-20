cb9549738961380d516b4aa247893f6748263345
Amrock <marcopiro91@gmail.com>
2023-07-20 17:05:15 +0200

    added exercise9

--- src/welcome.js
+++ src/welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component {
     render() {
         return  <div>
                     <p>Welcome, {this.props.name}</p>    
-                    { this.props.age && <Age age={this.props.age} />}
+                    { this.props.age > 18 && this.props.age < 65  && <Age age={this.props.age} />}
                 </div>
     }
 }
