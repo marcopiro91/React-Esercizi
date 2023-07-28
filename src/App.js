928e017b1e28be45c8263d3f7a65889da7bfd6ad
Amrock <marcopiro91@gmail.com>
2023-07-28 17:35:14 +0200

    exercise-17

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter"
+import { ClickTracker } from "./ClickTracker"
 
 export class App extends React.Component {
     render() {
-        return <ClickCounter />
+        return <ClickTracker />
     }
 }
 
