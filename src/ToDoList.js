b79168ad42fa2303e044fe67e4be5adddcb4bd3c
Amrock <marcopiro91@gmail.com>
2023-07-29 16:18:49 +0200

    exercise-26

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -24,11 +24,20 @@ export class ToDoList extends React.Component {
         this.setState({todos: []})
     }
 
+    handleRemove = (index) => {
+        this.setState((state) => {
+            state.todos.splice(index, 1)
+            return {
+                todos: state.todos
+            }
+        })
+    }
+
     render() {
             return  <div>
                         <ul>
                             {this.state.todos.map((item, index) => {
-                                return <li key={index}>{item}</li>
+                                return <li key={index}>{item}<button onClick={()=> {this.handleRemove(index)}}>remove</button></li>
                             })}
                         </ul>
                         <input ref={this._input} type="text" />
