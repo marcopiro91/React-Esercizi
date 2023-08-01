7d2d6ab951d09585b89794b4cd2421144591ca25
Amrock <marcopiro91@gmail.com>
2023-08-01 11:35:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { ToDoList } from "./ToDoList"
 
 export class App extends React.Component {
-    render() {
-        return <ToDoList render={
-            
-        }>
+    renderItems = (items, removeItems) => {
+        return items.map((item, index) => {
+             return <li key={index}>{item}<button onClick={() => removeItems(index)}>Remove</button></li>
+        })
+    }
 
-               </ToDoList>
+    render() {
+        return <ToDoList render={this.renderItems}/>
     }
 }
+
 
