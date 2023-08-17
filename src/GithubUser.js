0c4b7bc9d1639adb910a9f5fd91020dbe0703a93
Amrock <marcopiro91@gmail.com>
2023-08-17 16:14:32 +0200

    ok

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,11 +1,18 @@
 import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({ username }){
-    const data = useGithubUser(username)
+    const { data, loading, error, fetchDataGit } = useGithubUser()
+
+    function handleFetch(){
+        fetchDataGit(username)
+    }
 
     return (
         <div>
-            <h1>{data.name}</h1>
+            <button onClick={handleFetch}>fetch</button>
+            <h1>{data && data.name}</h1>
+            <h1>{loading && "Loading..."}</h1>
+            <h1>{error}</h1>
         </div>
     )
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,19 +1,28 @@
-import { useEffect, useState } from "react"
+import {  useState } from "react"
 
-export function useGithubUser(username){
-    let [data, setData] = useState({})
+export function useGithubUser(){
+    const [data, setData] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     async function fetchDataGit(username){
-        const response = await fetch(`https://api.github.com/users/${username}`)
-        if (response.status === 200){
-            const json = await response.json()
+        setLoading(true)
+        setError(null)
+        try{
+            const res = await fetch(`https://api.github.com/users/${username}`)
+            const json = await res.json()
+            if (res.status !== 200){
+                setError("User not found")
+            }
             setData(json)
+
+        } catch (error){
+            setError("Something went wrong")
+            setData({})
+        } finally {
+            setLoading(false)
         }
     }
 
-    useEffect(() => {
-        fetchDataGit(username)
-    }, [username])
-
-    return data
+    return { data, loading, error, fetchDataGit }
 }
