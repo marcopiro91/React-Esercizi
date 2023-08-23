f357193f01c4d6f569d8a379066a9c5ceeaf0738
Amrock <marcopiro91@gmail.com>
2023-08-23 15:47:03 +0200

    exercise-42

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Counter } from "./Counter";
+import { FilteredList } from "./FilteredList";
 
-
+const list = [
+ {name: 'Marco', id: 1, age: 31}, 
+ {name: "Liana", id: 2, age: 17},
+ {name: "Francesco", id: 3, age: 19}, 
+ {name: "Silvia", id: 4, age: 28}, 
+]
 
 export function App() {
-    return <Counter />
+    return <FilteredList list={list}/>
 }
 
 
