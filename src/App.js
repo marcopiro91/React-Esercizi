0086a5c437d67b5e5070b3299d9c896148beb0db
Amrock <marcopiro91@gmail.com>
2023-08-24 15:27:13 +0200

    exercise-47

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
+import { GithubUserList } from "./GithubUserList";
 
-
+/* Add a Route to the users path that shows the GihubUserList component. Modify it so that instead of showing the GithubUser component for each username entered, it shows a link to a nested route that shows the ShowGithubUser component. In doing so, remove the /users/:username route from the App component, and add a new nested route within the /users route. */
 
 
 
@@ -18,12 +19,15 @@ export function App() {
                 <li><Link to="/Counter">Link to Counter app</Link></li>
                 <li><Link to='/users/gianmarcotoso'>Link to my tutor's bio</Link></li>
                 <li><Link to='/'>Homepage</Link></li>
+                <li><Link to='/users'>users list</Link></li>
             </ul>
             
              <Routes>
                 <Route path="/" element={<Welcome />} />
                 <Route path="/Counter" element={<Counter initialValue={1}/>} />
-                <Route path="users/:username" element={<ShowGithubUser />} />
+                <Route path="/users" element={<GithubUserList/>}>
+                    <Route path=":username" element={<ShowGithubUser />} />
+                </Route>
                 <Route path="*" element={<div><h1>Not Found!</h1></div>} />
             </Routes> 
         </div>
