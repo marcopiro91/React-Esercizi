4b7b29a81da1684517314721553426df17357fa9
Amrock <marcopiro91@gmail.com>
2023-08-24 14:05:04 +0200

    exercise-47

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { Welcome } from "./Welcome";
+
 
 
 
 
 
 export function App() {
-    const navigate = useNavigate()
-
-    function handleButtonNavigate() {
-        navigate('./Counter')
-    }
+    
 
     return (
         <div>
-            <h1>Welcome to my application!</h1>
-            <button onClick={handleButtonNavigate}>Enter the counter app</button>
+            <ul>
+                <li><Link to="/Counter">Link to Counter app</Link></li>
+                <li><Link to='/users/gianmarcotoso'>Link to my tutor's bio</Link></li>
+                <li><Link to='/'>Homepage</Link></li>
+            </ul>
+            
              <Routes>
+                <Route path="/" element={<Welcome />} />
                 <Route path="/Counter" element={<Counter initialValue={1}/>} />
                 <Route path="users/:username" element={<ShowGithubUser />} />
             </Routes> 
---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export function Welcome () {
+    
+
+
+
+    return (
+        <div>
+            <h1>Welcome to my application!</h1>
+        </div>
+    )
+}
