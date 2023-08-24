77969aff277f3151aee86fe79c8ece03437d5dc0
Amrock <marcopiro91@gmail.com>
2023-08-24 16:40:10 +0200

    exercise-50

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,23 +1,19 @@
 
-
-import React, { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({username}) {
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => response.json())
-        .then(json => setData(json))
-        .catch(error => console.log(error))
-    }, [username])
+    const {data, error, isLoad} = useGithubUser(username)
 
 
 
     return (
         <div>
-            <p>Name: {data.login}</p>
-            <p>Bio: {data.bio}</p>
+            {isLoad && <h1>Loading...</h1>}
+            {error && <h1>Errror: {error}</h1>}
+            { data && <div>
+                <p>Name: {data.login}</p>
+                <p>Bio: {data.bio}</p>
+            </div>}
         </div>
         
     )
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import  useSWR  from "swr"
+
+const fetcher = url => fetch(url).then(response => response.json())
+
+export function useGithubUser(username){
+    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
 
-export function useGithubUser(username) {
-    const [data, setData] = useState(null)
 
-    
-
-    return data
+    return {
+        data: data,
+        error,
+        isLoad: !data && !error
+    }
 }
+
+/* Modify the useGithubUser custom hook from to use the useSWR hook to fetch the data of a Github user. */
